ddc442ffdd0092ed1ac31434a068ecd84145a9d8
abuduj3na <meric.ucar@icloud.com>
2024-09-28 21:22:49 +0200

    oefening

--- Kassa/MainWindow.xaml.cs
+++ Kassa/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,29 @@ namespace Kassa
         public MainWindow()
         {
             InitializeComponent();
+        }
+        double price;
+        double amount;
+        double toPay;
+
+        private void calculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            price = double.Parse(priceTextBox.Text);
+            amount = double.Parse(amountTextBox.Text);
+            toPay = price * amount;
+            amountToPayTextBox.Text = $"{toPay}";
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            priceTextBox.Text = "0";
+            amountTextBox.Text = "0";
+            amountToPayTextBox.Text ="0";
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
